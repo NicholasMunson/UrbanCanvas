045ba16d1e77b2a5d920179fff45146daab06a74
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-06-26 14:36:12 -0600

    it's routing??

--- frontEnd/urbancanvas/src/Components/Card.js
+++ frontEnd/urbancanvas/src/Components/Card.js
@@ -3,22 +3,22 @@ import React from 'react';
 const Card = () => {
     let style = {
         backgroundColor: "black",
-        width:"60%"
+        width: "60%"
 
     }
-    let cardStyle ={
+    let cardStyle = {
         width: "80%",
-        height:"auto",
-        padding:"10%"
+        height: "auto",
+        padding: "10%"
     }
-    let ptag ={
-       color:"white"
+    let ptag = {
+        color: "white"
     }
-    return(
+    return (
         <section style={style} >
-            <img style={cardStyle} src="https://lh5.ggpht.com/jyCBS54ZBvqCks_Uy7yXzsfXDYoSNg52TpsWG6Ta0ATu7wKr2b8mWuv7YfSg" />
+            <img style={cardStyle} src="https://lh5.ggpht.com/jyCBS54ZBvqCks_Uy7yXzsfXDYoSNg52TpsWG6Ta0ATu7wKr2b8mWuv7YfSg" alt="art" />
             <p style={ptag}>
-            Location
+                Location
             </p>
         </section>
     )
--- frontEnd/urbancanvas/src/Components/Header.js
+++ frontEnd/urbancanvas/src/Components/Header.js
@@ -1,28 +1,13 @@
-// import React from 'react';
-// import { Button } from 'semantic-ui-react';
+import React from 'react';
 
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Urban Canvas</h1>
 
-// const Header = (props) => {
-//     return (
-//         <header>
-//             <h1>Urban Canvas</h1>
-//             <Button icon='bars' />
-//             <img />
+    </header>
 
-//         </header>
-//     )
-// }
+  )
+}
 
-// export default Header;
-
-import React from 'react'
-import { Header, Button } from 'semantic-ui-react'
-
-const HeaderExampleBlock = () => (
-  <Header block>
-    <h3>Urban Canvas </h3>
-    <Button icon='bars' />
-  </Header>
-)
-
-export default HeaderExampleBlock
+export default Header
