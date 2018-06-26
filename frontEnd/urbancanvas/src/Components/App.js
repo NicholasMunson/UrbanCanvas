ab5ded4ffbb302a838b81073713a5e2b17bf0c31
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-26 13:07:09 -0600

    added card

--- frontEnd/urbancanvas/src/Components/App.js
+++ frontEnd/urbancanvas/src/Components/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import HeaderExampleBlock from './Header';
 import Welcome from './Welcome';
+import Card from './Card';
+
 
 class App extends Component {
     render() {
         return (<div className="App">
             <HeaderExampleBlock />
             <Welcome />
+            <Card />
 
         </div>);
     }
