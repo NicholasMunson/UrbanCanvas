f9065d5646684679ec834afd77296b9affa94136
missmeow71 <sensuousdesigner@gmail.com>
2018-06-26 11:01:49 -0600

    created header & installed semantic ui

---
+++ frontEnd/urbancanvas/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1>Urban Canvas</h1>
+            <img />
+    
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Header;
