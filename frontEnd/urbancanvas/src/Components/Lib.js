3342ffeb51d8297d7181bf347a688fee6d44d103
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-27 11:21:36 -0600

    made lib comp stateful

--- frontEnd/urbancanvas/src/Components/Lib.js
+++ frontEnd/urbancanvas/src/Components/Lib.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Card from "./Card";
 
+
+
+
 class Lib extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      display: "a",
+      artList:[]
+    }
+  }
   render() {
     return (<div className="App">
-
+    
       <Card />
 
     </div>);
