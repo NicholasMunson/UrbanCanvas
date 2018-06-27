7100537554181c0ba895569fa6d0738516a4e7d9
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-27 14:38:57 -0600

    added the Directions

--- frontEnd/urbancanvas/src/Components/Lib.js
+++ frontEnd/urbancanvas/src/Components/Lib.js
@@ -1,34 +1,34 @@
 import React, {Component} from 'react'
 import List from "./List"
-import Map from "./Map"
+// import Map from "./Map"
 import Multi from "./Multi"
 
 class Lib extends Component {
-		constructor(props) {
-				super(props)
-				this.state = {
-						display: "a",
-						artList: [],
-						id: null
-				}
-		}
-		componentWillMount = () => {
-				let URL = "./dumb.json"
-				fetch(URL)
-						.then(res => res.json())
-						.then(res => {
-								this.setState({artList: res})
-						})
-		}
-		render() {
-				return (
-						<div className="card-lib">
-								<List artList={this.state.artList}/>
-								<Map id={this.state.id} artList={this.state.artList}/>
-								<Multi display={this.state.display}/>
-						</div>
-				)
-		}
+    constructor(props) {
+        super(props)
+        this.state = {
+            display: "a",
+            artList: [],
+            id: null
+        }
+    }
+    componentWillMount = () => {
+        let URL = "./dumb.json"
+        fetch(URL)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({artList: res})
+            })
+    }
+    render() {
+        return (
+            <div className="card-lib">
+                <List artList={this.state.artList}/> 
+                {/* <Map id={this.state.id} artList={this.state.artList}/> */}
+                <Multi display={this.state.display}/>
+            </div>
+        )
+    }
 }
 
 export default Lib
