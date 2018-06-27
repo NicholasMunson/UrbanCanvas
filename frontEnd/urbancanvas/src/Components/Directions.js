25e6bebf4d1df32faf7f0dd5ecb07abfdf5dc161
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-27 14:18:09 -0600

    fixed spacing issues

---
+++ frontEnd/urbancanvas/src/Components/Directions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Directions = (props) => {
+    return (
+        <p>
+				When you select an art piece that you would like to visit the map will load
+        on the top and show the location
+        </p>
+    )
+
+}
+
+export default Directions
--- frontEnd/urbancanvas/src/Components/List.js
+++ frontEnd/urbancanvas/src/Components/List.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ArtCards from "./ArtCards"
 
 const List = (props) => {
-  const ArtPost = props.artList.map(card => <ArtCards card={card} key={card.id} />)
-  return (
-    <div className="art-list">
-      {ArtPost}
-    </div>
-  )
+    const ArtPost = props
+        .artList
+        .map(card => <ArtCards card={card} key={card.id}/>)
+    return (<div className="art-list">
+        {ArtPost}
+    </div>)
 }
 
 export default List
