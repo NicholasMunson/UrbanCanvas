dc328bc46251c45499ac0df2c7923855f5c6ed67
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-27 14:24:03 -0600

    testing settings

--- frontEnd/urbancanvas/src/Components/ArtCards.js
+++ frontEnd/urbancanvas/src/Components/ArtCards.js
@@ -2,17 +2,17 @@ import React from 'react'
 import "./App.css"
 
 const ArtCards = (props) => {
-    console.log(props)
-    return (
-        <div className="card" id={props.card.id} key={props.card.id}>
-            <img className="card-img" src={props.card.imgUrl} alt={props.card.id}/>
-            <p className="card-location">{props.card.description}</p>
-            <div className="card-buttons">
-                <button className="edit-button">Edit</button>
-                <button className="delete-button" s="s">Delete</button>
-            </div>
-        </div>
-    )
+	console.log(props)
+	return (
+		<div className="card" id={props.card.id} key={props.card.id}>
+			<img className="card-img" src={props.card.imgUrl} alt={props.card.id}/>
+			<p className="card-location">{props.card.description}</p>
+			<div className="card-buttons">
+				<button className="edit-button">Edit</button>
+				<button className="delete-button" s="s">Delete</button>
+			</div>
+		</div>
+	)
 }
 
 export default ArtCards
