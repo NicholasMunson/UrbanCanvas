807294977e2678236400719102e71c5e69261526
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-27 10:11:20 -0600

    removed unused code from app.js

--- frontEnd/urbancanvas/src/Components/App.js
+++ frontEnd/urbancanvas/src/Components/App.js
@@ -1,57 +1,12 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom'
 import Header from './Header'
 import Lib from './Lib';
 import Welcome from './Welcome';
 import "./App.css"
-
-// const Home = () => (
-//   <div>
-//     <Welcome />
-//   </div>
-// )
-
-// const Art = () => (
-//   <Lib />
-// )
-
-// const Topic = ({ match }) => (
-//   <div>
-//     <h3>{match.params.topicId}</h3>
-//   </div>
-// )
-
-// const Topics = ({ match }) => (
-//   <div>
-//     <h2>Topics</h2>
-//     <ul>
-//       <li>
-//         <Link to={`${match.url}/rendering`}>
-//           Rendering with React
-//         </Link>
-//       </li>
-//       <li>
-//         <Link to={`${match.url}/components`}>
-//           Components
-//         </Link>
-//       </li>
-//       <li>
-//         <Link to={`${match.url}/props-v-state`}>
-//           Props v. State
-//         </Link>
-//       </li>
-//     </ul>
-
-//     <Route path={`${match.path}/:topicId`} component={Topic} />
-//     <Route exact path={match.path} render={() => (
-//       <h3>Please select a topic.</h3>
-//     )} />
-//   </div>
-// )
 
 const App = () => (
   <Router>
