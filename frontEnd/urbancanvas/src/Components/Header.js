0d80c1e28abcdaf7cf0233e933ec98eca47ce758
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-28 15:37:38 -0600

    added css from sandy

--- frontEnd/urbancanvas/src/Components/Header.js
+++ frontEnd/urbancanvas/src/Components/Header.js
@@ -3,18 +3,21 @@ import {Link} from 'react-router-dom'
 
 const Header = (props) => {
     return (
-        <header className="header App-header">
-            <h1 className="App-title">
-                Urban Canvas
-            </h1>
-            <button>
-                <Link to="/">Home</Link>
-            </button>
-            <button>
-                <Link to="/Art">Art</Link>
-            </button>
-        </header>
+        <React.Fragment>
+            <header className="App-header">
+                <h1 className="App-title">
+                    Urban Canvas
+                </h1>
+            </header>
+            <div className="nav">
+                <button className="navbut1">
+                    <Link className="home" to="/">Home</Link>
+                </button>
+                <button className="navbut2">
+                    <Link className="art" to="/Art">Art</Link>
+                </button>
+            </div>
+        </React.Fragment>
     )
 }
-
 export default Header
--- frontEnd/urbancanvas/src/Components/Welcome.js
+++ frontEnd/urbancanvas/src/Components/Welcome.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Welcome extends Component {
-  render() {
-    return (
-      <section className="welcome">
-        <h1>Urban Canvas</h1>
-        <p>Welcome to Urban Canvas the app where you can discover new and exciting
-            beautiful works of art located in non-traditional spaces throughout the city.
-                </p>
-        <button>
-          <Link to="/Art">Begin your adventure!</Link>
-        </button>
-      </section>
-    );
-  }
+    render() {
+        return (
+            <div className="background-img">
+                <section className="welcome">
+                    <h1 className="subtitle">
+                        Urban Canvas
+                    </h1>
+                    <p>Welcome to Urban Canvas the app where you can discover new and exciting
+                        beautiful works of art located in non-traditional spaces throughout the city.
+                    </p>
+
+                    <Link className="goto" to="/Art">
+                        < button className="enter">
+                            Begin Your Adventure!</button>
+                    </Link>
+
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Welcome
