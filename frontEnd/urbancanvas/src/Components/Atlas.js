c47cad7a712366b4b3daee296f7ab266f83ddcf4
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-06-28 10:26:18 -0600

    added recompose to npm for maps

--- frontEnd/urbancanvas/src/Components/Atlas.js
+++ frontEnd/urbancanvas/src/Components/Atlas.js
@@ -4,6 +4,7 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow
 } from "react-google-maps";
 
 
@@ -15,6 +16,43 @@ const Atlas = withScriptjs(withGoogleMap(props =>
     {props.isMarkerShown && <Marker position={{ lat: 39.7576749, lng: -105.006952 }} />}
   </GoogleMap>
 ));
+
+
+// const Atlas = withScriptjs(withGoogleMap(
+//   withStateHandlers(() => ({
+//     isOpen: false,
+//   }), {
+//       onToggleOpen: ({ isOpen }) => () => ({
+//         isOpen: !isOpen,
+//       })
+//     }),
+//   withScriptjs,
+//   withGoogleMap
+// )(props =>
+//   <GoogleMap
+//     defaultZoom={8}
+//     defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//   >
+//     <Marker
+//       position={{ lat: -34.397, lng: 150.644 }}
+//       onClick={props.onToggleOpen}
+//     >
+//       {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+//         <FaAnchor />
+//       </InfoWindow>}
+//     </Marker>
+//   </GoogleMap>
+// )
+
+
+
+
+
+
+
+
+
+
 
 export default Atlas;
 
