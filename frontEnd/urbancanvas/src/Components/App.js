8b0c709d14974250e1f57d5785fb434435146598
Brevon Good <brevonbgood@comcast.net>
2018-06-28 10:08:55 -0600

    update App to stateful move functionality from Lib to App

--- frontEnd/urbancanvas/src/Components/App.js
+++ frontEnd/urbancanvas/src/Components/App.js
@@ -8,17 +8,37 @@ import Lib from './Lib';
 import Welcome from './Welcome'
 import "./App.css"
 
-const App = () => (
-  <Router>
-    <React.Fragment>
-      <Route className="header" path="/" component={Header} />
-      <div className="app">
-        <Route exact path="/" component={Welcome} />
-        <Route path="/art" component={Lib} />
-      </div>
-    </React.Fragment>
-  </Router>
-)
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          display: "a",
+          artList: [],
+          id: null
+      }
+  }
+  componentWillMount = () => {
+      let URL = "https://urban-canvas-server.herokuapp.com/api/v1/art-cards"
+      fetch(URL)
+          .then(res => res.json())
+          .then(res => {
+              this.setState({ artList: res })
+          })
+  }
+
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Route className="header" path="/" component={Header} />
+          <div className="app">
+            <Route exact path="/" component={Welcome} />
+            <Route path="/art" component={() => <Lib display={this.state.display} artList={this.state.artList} id={this.state.id} />} />
+          </div>
+        </React.Fragment>
+      </Router>
+    )
+  }
+}
+
 export default App
-
-
--- frontEnd/urbancanvas/src/Components/Lib.js
+++ frontEnd/urbancanvas/src/Components/Lib.js
@@ -4,31 +4,15 @@ import Map from "./Map"
 import Multi from "./Multi"
 
 class Lib extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            display: "a",
-            artList: [],
-            id: null
-        }
-    }
-    componentWillMount = () => {
-        let URL = "https://urban-canvas-server.herokuapp.com/api/v1/art-cards"
-        fetch(URL)
-            .then(res => res.json())
-            .then(res => {
-                this.setState({ artList: res })
-            })
-    }
     render() {
         return (
             <div className="card-lib">
-                <List artList={this.state.artList} />
-                <Map id={this.state.id} artList={this.state.artList} />
+                <List artList={this.props.artList} />
+                <Map id={this.props.id} artList={this.props.artList} />
                 <Multi
-                    display={this.state.display}
-                    artList={this.state.artList}
-                    id={this.state.id} />
+                    display={this.props.display}
+                    artList={this.props.artList}
+                    id={this.props.id} />
             </div>
         )
     }
