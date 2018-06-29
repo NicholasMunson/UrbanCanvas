1b757ac06211d64a8c595ce3af6186def09fbb73
missmeow71 <sensuousdesigner@gmail.com>
2018-06-29 14:26:33 -0600

    changed class to className

--- frontEnd/urbancanvas/src/Components/Header.js
+++ frontEnd/urbancanvas/src/Components/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Header = (props) => {
     return (
-        <div class="header">
+        <div className="header">
             <header className="App-header">
                 <h1 className="App-title">
                     Urban Canvas
