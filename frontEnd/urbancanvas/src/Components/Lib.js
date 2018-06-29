16fd4f705a3c107b831aef1a74909bd2e63c3334
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-29 10:55:12 -0600

    added multi css and imported it to the componunets

--- frontEnd/urbancanvas/src/Components/Lib.js
+++ frontEnd/urbancanvas/src/Components/Lib.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import List from "./List"
 import Map from "./Map"
 import Multi from "./Multi"
+import "../Components/Multi.css"
 
 class Lib extends Component {
     render() {
         return (
-            <div className="card-lib">
-                <List handleClick={this.props.handleClick} artList={this.props.artList} handleArtDelete={this.props.handleArtDelete} />
-                <Map currentArt={this.props.currentArt} />
-                <Multi
-                    handleCreateArtCard={this.props.handleCreateArtCard}
-                    display={this.props.display}
-                    currentArt={this.props.currentArt} />
+            <div className="page-2-container">
+                <div className="card-lib">
+                    <List
+                        handleClick={this.props.handleClick}
+                        artList={this.props.artList}
+                        handleArtDelete={this.props.handleArtDelete}/>
+                    <Map currentArt={this.props.currentArt}/>
+                    <Multi
+                        handleCreateArtCard={this.props.handleCreateArtCard}
+                        display={this.props.display}
+                        currentArt={this.props.currentArt}/>
+                </div>
             </div>
         )
     }
