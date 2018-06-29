1516797ce9ae006a5d93b45f943e6aaec5d2c4a7
Brevon Good <brevonbgood@comcast.net>
2018-06-29 15:02:25 -0600

    add cypress tests

---
+++ frontEnd/urbancanvas/cypress/integration/spec.js
@@ -0,0 +1,31 @@
+describe('End to end tests!!', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('.enter').click()
+  })
+  it('displays art cards', () => {
+    cy.get('.card').should('have.length.gt', 0)
+  })
+  xit('it can create art card', () => {
+    cy.get('.add').click()
+
+    cy.get('input[name="imgUrl"]').type('http://via.placeholder.com/500x500')
+    cy.get('textarea[name="description"]').type('Test Art')
+    cy.get('input[name="location"]').type('1234 S. Test St.')
+    cy.get('input[name="lat"]').type('0')
+    cy.get('input[name="lng"]').type('0')
+    cy.get('input[type="submit"]').click()
+  })
+  xit('it can update art card', () => {
+    cy.get('.card').first().within(($card => {
+      cy.get('.edit-')
+    }))
+
+    cy.get('textarea[name="description"]').type('Test Art Update')
+    cy.get('input[type="submit"]').click()
+  })
+  xit('it can delete art card', () => {
+    cy.get('.card')
+  })
+  
+})
