bfe5dd1f6bd0f93064cf5e487a31f9c5dd075d37
missmeow71 <sensuousdesigner@gmail.com>
2018-06-29 12:13:56 -0600

    added classes to ArtCards

--- frontEnd/urbancanvas/src/Components/ArtCards.js
+++ frontEnd/urbancanvas/src/Components/ArtCards.js
@@ -7,9 +7,10 @@ const ArtCards = (props) => {
 		<div onClick={(event) => props.handleClick(event, props.card.id)} className="card" id={props.card.id} key={props.card.id}>
 			<img className="card-img" src={props.card.imgUrl} alt={props.card.id}/>
 			<p className="card-location">{props.card.location}</p>
-			<div className="card-buttons">
-				<button className="edit-button">Edit</button>
-				<button onClick={(event) => props.handleArtDelete(event, props.card.id)} className="delete-button" s="s">Delete</button>
+			<div className="card-buttons button-container">
+				<button className=" edit- buttons navbut button-style">Edit</button>
+				<button onClick={(event) => props.handleArtDelete(event, props.card.id)} 
+				className="delete-button navbut button-style" s="s">Delete</button>
 			</div>
 		</div>
 	)
