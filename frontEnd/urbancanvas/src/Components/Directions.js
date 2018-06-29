353181153ee6a1a5b112a270390ad347066e0a20
missmeow71 <sensuousdesigner@gmail.com>
2018-06-29 15:54:43 -0600

    changed format of text

--- frontEnd/urbancanvas/src/Components/Directions.js
+++ frontEnd/urbancanvas/src/Components/Directions.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Directions = (props) => {
     return (
-        <p>
+        <p className="directions">
 		When you select an art piece that you would like to visit, the map will load
         on the top and show the location. Scroll down to see all art uploaded to Urban Canvas.
         Add a newly found art piece by clicking the add button below. Edit or delete a post by selecting
